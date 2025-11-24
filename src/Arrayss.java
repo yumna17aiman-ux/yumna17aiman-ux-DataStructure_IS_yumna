0aaae17de414e8b490f91666d2e623e4c5773803
yumna <yumna17aiman@gmail.com>
2025-11-24 10:25:20 +0300

    lab4 -Arrays-part2

--- src/Arrayss.java
+++ src/Arrayss.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class Arrayss {
+    Scanner input=new Scanner(System.in);
+
+    public void Travesal(int[]a) {
+
+        for(int i=0;i<a.length;i++)
+            System.out.println("index["+i+"]"+"="+a[i]);
+    }
+
+
+    public void Travesal2(int[]a) {
+        for(int e :a)
+            System.out.println(e);
+    }
+
+    public void input (int []a){
+        System.out.println("enter "+a.length+" numbers");
+        for(int i=0;i<a.length;i++)
+            a[i]=input.nextInt();
+    }
+
+    public void update(int []a ,int index,int newvalue){
+        if(index<0 || index >=a.length)
+            System.out.println("Index not found");
+        else
+            a[index]=newvalue;
+    }
+
+    public void delate(int []a ,int index){
+        if(index<0 || index >=a.length)
+            System.out.println("Index not found");
+        else
+            a[index]=0;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int []numbers={1,2,3,4,6,7,9};
-        int []numbers2= new int [8];
-        numbers2[3]=4;
 
-        for(int i=0;i<numbers.length;i++)
-            System.out.println("index["+i+"]"+"="+numbers[i]);
+        Arrayss arr=new Arrayss();
+
+       int []numbers={1,2,3,5,4};
+
+        int []numbers2= new int[8];
+
+          arr.Travesal(numbers);
+        //arr.input(numbers2);
+        arr.delate(numbers,4);
+        arr.update(numbers,3,9);
     }
 }
