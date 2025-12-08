30d9fe9047377895a64c755501bca6690ea89c83
yumna <yumna17aiman@gmail.com>
2025-12-08 10:04:25 +0300

    lab6 -Arrays-part&singleLinkedList

--- src/SinglyLinkedList/main.java
+++ src/SinglyLinkedList/main.java
@@ -15,14 +15,21 @@ class Node<E> {
         private E data;
         private Node<E> next;
 
+    public Node<E> getNext() {
+        return next;
+    }
 
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
 
+    public E getData() {
+        return data;
+    }
 
-
-
-
-
-
+    public void setData(E data) {
+        this.data = data;
+    }
 
     class Node<E>{
 
