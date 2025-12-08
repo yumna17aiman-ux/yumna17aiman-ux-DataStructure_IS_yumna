4f3244ec61d9c675f3557b8f7fde573ee6466d2e
yumna <yumna17aiman@gmail.com>
2025-12-08 10:06:34 +0300

    lab6 -Arrays-part&singleLinkedList

--- src/SinglyLinkedList/SinglyLinkedList.java
+++ src/SinglyLinkedList/SinglyLinkedList.java
@@ -1,4 +1,44 @@
 package SinglyLinkedList;
 
-public class SinglyLinkedList {
-}
+
+public class main {
+
+    public class SinglyLinkedList<E> {
+        private Node<E> head=null;
+        private Node<E> tail=null;
+        private int size=0;
+
+        public int size (){ return size; }
+        public boolean isEmpty() {
+            return size()==0;}
+    }
+    class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        class Node<E>{
+
+        }
+
+
+
+
+
+    }
+
